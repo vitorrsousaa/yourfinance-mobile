06a7343a3d41df37fdc8ef5066ada1b1ce9e99f9
Vitor Sousa <vitor.silva@vetta.digital>
2023-05-12 20:25:28 -0300

    bugfix/goalbox: updated types

--- src/hooks/useGoals.ts
+++ src/hooks/useGoals.ts
@@ -1,21 +1,21 @@
 import { useQuery } from 'react-query';
 
 import GoalsService from '../service/GoalsService';
-import { TGoalResponse } from '../types/Goal';
+import { TGoal } from '../types/Goal';
 
 export function useGoals(): {
-  goals: TGoalResponse[];
+  goals: TGoal[];
   isErrorGoals: boolean;
   isLoadingGoals: boolean;
   refetch: () => Promise<unknown>;
-  } {
+} {
   const {
     data: goalsResponse,
     isError,
     isLoading,
     isFetching,
     refetch,
-  } = useQuery<TGoalResponse[]>({
+  } = useQuery<TGoal[]>({
     queryKey: ['@goals'],
     queryFn: () => GoalsService.list(),
     staleTime: 1000 * 60 * 30, // 30 minutos
