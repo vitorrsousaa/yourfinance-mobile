e23f8481327fc6c1945d90de65b55b161b5e25e5
Vitor Sousa <vitor.silva@vetta.digital>
2023-05-12 20:24:02 -0300

    bugfix/goalbox: fixes types

--- src/types/Goal/HistoricTransaction.ts
+++
@@ -1,8 +0,0 @@
-import { TModeTransaction } from './modeTransaction';
-
-export interface THistoricTransaction {
-  _id: string;
-  date: string;
-  amount: number;
-  modeTransaction: TModeTransaction;
-}
