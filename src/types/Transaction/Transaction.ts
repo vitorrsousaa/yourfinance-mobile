896038d4ef96adec34b194dd9298265e1dca5326
Vitor Sousa <v.sousa.cf@gmail.com>
2023-05-24 15:44:39 -0300

    feat/create-transaction: Updated type transaction

--- src/types/Transaction/Transaction.ts
+++ src/types/Transaction/Transaction.ts
@@ -4,23 +4,10 @@ import { TModality } from '../Modality';
 export interface TTransaction {
   id: string;
   user: string;
-  modality: TModality;
   description: string;
   amount: number;
+  modality: TModality;
   category: TCategory;
-  type: string;
-  date: Date;
-  updatedAt: Date;
-  createdAt: Date;
-}
-
-export interface TTransactionPersistance {
-  id: string;
-  userId: string;
-  modalityId: TModality;
-  name: string;
-  amount: number;
-  categoryId: TCategory;
   type: string;
   date: Date;
   updatedAt: Date;
