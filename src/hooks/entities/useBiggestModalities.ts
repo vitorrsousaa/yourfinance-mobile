de5f67dbe3775fbc7cac58432a999684bdd04092
Vitor Sousa <vitor.silva@vetta.digital>
2023-05-29 19:26:47 -0300

    feat: Created hook to share biggest modality with all application

---
+++ src/hooks/entities/useBiggestModalities.ts
@@ -0,0 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+
+import AnalyticsService from '../../service/AnalyticsService';
+import { TBiggestModality } from '../../types/Analytics';
+
+export default function useBiggestModalities(): {
+  hasErrorBiggestModalities: boolean;
+  isLoadingBiggestModalities: boolean;
+  biggestModalities: TBiggestModality | null;
+  refetch: () => Promise<unknown>;
+} {
+  const {
+    data: biggestModalities,
+    isError,
+    isLoading,
+    refetch,
+  } = useQuery<TBiggestModality>({
+    queryKey: ['@biggestModalities'],
+    queryFn: () => AnalyticsService.getBiggestModalities(),
+    staleTime: 1000 * 60 * 30, // 30 minutos
+    cacheTime: 1000 * 60 * 20, // 20 minutos
+  });
+
+  return {
+    refetch,
+    isLoadingBiggestModalities: isLoading,
+    hasErrorBiggestModalities: isError,
+    biggestModalities: biggestModalities || null,
+  };
+}
