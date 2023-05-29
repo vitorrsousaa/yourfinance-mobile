19385cea6f31da14a34b3d74eb5f3afc3f9ec706
Vitor Sousa <vitor.silva@vetta.digital>
2023-05-29 17:18:47 -0300

    feat: Includes modality name to render Transactions, and added methods in view model

--- src/components/LastTransactions/components/Transaction/Transaction.view-model.tsx
+++ src/components/LastTransactions/components/Transaction/Transaction.view-model.tsx
@@ -1,5 +1,28 @@
-export interface TransactionViewModelProps {}
+import { useTheme } from 'styled-components/native';
+
+import Icon from '../../../Icons';
+
+export interface TransactionViewModelProps {
+  getColor: (name: string) => string;
+  getIcon: (category: string) => JSX.Element;
+}
 
 export function TransactionViewModel() {
-  return {};
+  const { colors } = useTheme();
+
+  function getColor(name: string) {
+    if (name === 'Receitas') {
+      return colors.green[400];
+    } else {
+      colors.red[400];
+    }
+  }
+
+  function getIcon(category: string) {
+    if (category === 'Receitas') {
+      return <Icon name="income" testID="income-icon" />;
+    }
+    return <Icon name="outcome" testID="outcome-icon" />;
+  }
+  return { getColor, getIcon };
 }
