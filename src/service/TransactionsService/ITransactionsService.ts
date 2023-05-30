ec4a73c7cd98784852a009ea0a198aa309efdc14
Vitor Sousa <vitor.silva@vetta.digital>
2023-05-30 14:58:41 -0300

    feat: Includes services to delete transaction

--- src/service/TransactionsService/ITransactionsService.ts
+++ src/service/TransactionsService/ITransactionsService.ts
@@ -3,4 +3,5 @@ import { TTransaction, TTransactionCreate } from '../../types/Transaction';
 export interface ITransactionsService {
   list(): Promise<TTransaction[]>;
   create(transaction: TTransactionCreate): Promise<TTransaction>;
+  delete(id: string): Promise<void>;
 }
