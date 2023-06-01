944de3c64a27f283a3ea27e2412652552b37c853
Vitor Sousa <vitor.silva@vetta.digital>
2023-06-01 11:40:28 -0300

    fix: welcome tests

--- src/components/Welcome/Welcome.spec.tsx
+++ src/components/Welcome/Welcome.spec.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
 import { render } from '@testing-library/react-native';
 
-import 'jest-styled-components';
 import ThemeProvider from '../ThemeProvider';
+
 import Welcome from './Welcome';
+
+import 'jest-styled-components';
 
 describe('Welcome', () => {
   it('Should render correctly with default props', () => {
