d8219acd1326db212b684928b1456d0bc20c13b8
Vitor Sousa <vitor.silva@vetta.digital>
2023-06-02 12:33:40 -0300

    fix: fix tests

--- src/screens/Goals/components/ContinuousButton/ContinuousButton.spec.tsx
+++ src/screens/Goals/components/ContinuousButton/ContinuousButton.spec.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react-native';
-import { View } from 'react-native';
 
 import ThemeProvider from '../../../../components/ThemeProvider';
 
 import ContinuousButton from './ContinuousButton';
 
 import 'jest-styled-components';
-
-jest.mock('styled-components/native', () => {
-  return {
-    useTheme: jest.fn().mockReturnValue({
-      colors: {
-        black: {
-          600: '#000',
-        },
-      },
-    }),
-  };
-});
 
 describe('Continuous Button', () => {
   it('Should render component When called with default props', () => {
@@ -35,22 +22,7 @@ describe('Continuous Button', () => {
     rendered.getByTestId('arrowRight');
   });
 
-  it('Should be disabled when isValid is true', () => {
-    // Arrange
-    const onPress = jest.fn();
-    const rendered = render(
-      <ThemeProvider>
-        <ContinuousButton isValid onPress={onPress} />
-      </ThemeProvider>
-    );
-
-    // Act
-    fireEvent.press(rendered.getByTestId('arrowRight'));
-
-    // Assert
-    expect(onPress).not.toHaveBeenCalled();
-  });
-  it('Should be enabled when isValid is false', () => {
+  it('Should be enabled when isValid is true', () => {
     // Arrange
     const onPress = jest.fn();
     const rendered = render(
@@ -64,5 +36,20 @@ describe('Continuous Button', () => {
 
     // Assert
     expect(onPress).toHaveBeenCalled();
+  });
+  it('Should be disabled when isValid is false', () => {
+    // Arrange
+    const onPress = jest.fn();
+    const rendered = render(
+      <ThemeProvider>
+        <ContinuousButton isValid={false} onPress={onPress} />
+      </ThemeProvider>
+    );
+
+    // Act
+    fireEvent.press(rendered.getByTestId('arrowRight'));
+
+    // Assert
+    expect(onPress).not.toHaveBeenCalled();
   });
 });
