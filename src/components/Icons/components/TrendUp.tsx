6067b26be7ed22a13f87b4ca050376c2d877abd1
Vitor Sousa <v.sousa.cf@gmail.com>
2023-06-07 07:52:26 -0300

    feat: Updated props in svg icon

--- src/components/Icons/components/TrendUp.tsx
+++ src/components/Icons/components/TrendUp.tsx
@@ -1,10 +1,10 @@
-import { SvgXml } from 'react-native-svg';
+import { SvgProps, SvgXml } from 'react-native-svg';
 
-interface TrendUpProps {
+interface TrendUpProps extends SvgProps {
   color?: string;
 }
 
-export function TrendUp({ color }: TrendUpProps) {
+export function TrendUp({ color, ...props }: TrendUpProps) {
   const markup = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none"><path stroke="${
     color ? color : '#28C140'
   }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m14.398 3.5-6 6-2.5-2.5-4.5 4.5"/><path stroke="${
@@ -12,5 +12,5 @@ export function TrendUp({ color }: TrendUpProps) {
   }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.398 7.5v-4h-4"/></svg>
   `;
 
-  return <SvgXml xml={markup} />;
+  return <SvgXml xml={markup} {...props} />;
 }
