48ffb80e32f42fd4df5e0200b7079ae8f455dfa0
Vitor Sousa <v.sousa.cf@gmail.com>
2023-06-07 07:52:06 -0300

    feat: Created tests for category summary

--- src/screens/Home/components/CategorySummary/CategorySummary.view-model.tsx
+++ src/screens/Home/components/CategorySummary/CategorySummary.view-model.tsx
@@ -1,30 +1,42 @@
-import { useMemo } from 'react';
+import Icon from '../../../../components/Icons';
+import formatAmount from '../../../../utils/formatAmout';
 
 import { ViewModelProps } from './CategorySummary';
 
 export interface CategorySummaryViewModelProps {
   categoryName: string;
-  currentMonth: number;
-  percent: number;
-  difference: number;
+  currentMonth: string;
+  percent: string;
+  difference: string;
+  getIcon: (color: string) => JSX.Element;
 }
 
 export function CategorySummaryViewModel(data: ViewModelProps) {
-  const categoryName = useMemo(
-    () => (data.categoryName ? data.categoryName : '-'),
-    []
-  );
-  const currentMonth = useMemo(
-    () => (data.currentMonth ? data.currentMonth : 0),
-    []
-  );
-  const percent = useMemo(() => (data.percent ? data.percent : 0), []);
-  const difference = useMemo(() => (data.difference ? data.difference : 0), []);
+  const categoryName = data.categoryName ? data.categoryName : '-';
+
+  const currentMonth = data.currentMonth
+    ? formatAmount(data.currentMonth)
+    : formatAmount(0);
+
+  const percent = data.percent ? `${Math.floor(data.percent)}%` : '0%';
+
+  const difference = data.difference
+    ? formatAmount(data.difference)
+    : formatAmount(0);
+
+  function getIcon(color: string) {
+    if (data.percent < 0) {
+      return <Icon name="trendDown" testID="trend-down" color={color} />;
+    } else {
+      return <Icon name="trendUp" testID="trend-up" color={color} />;
+    }
+  }
 
   return {
     currentMonth,
     categoryName,
     percent,
     difference,
+    getIcon,
   };
 }
--- src/screens/Home/components/CategorySummary/CategorySummary.view.tsx
+++ src/screens/Home/components/CategorySummary/CategorySummary.view.tsx
@@ -1,8 +1,6 @@
 import { useTheme } from 'styled-components/native';
 
-import Icon from '../../../../components/Icons';
 import { Text } from '../../../../components/Text';
-import formatAmount from '../../../../utils/formatAmout';
 
 import { CategorySummaryViewProps } from './CategorySummary';
 import * as styled from './CategorySummary.styles';
@@ -15,7 +13,9 @@ interface Props {
 
 export function CategorySummaryView({ viewModel, props }: Props) {
   const { ...categorySummaryProps } = props;
-  const { currentMonth, categoryName, percent, difference } = viewModel;
+  const { currentMonth, categoryName, percent, difference, getIcon } =
+    viewModel;
+
   const { colors } = useTheme();
 
   const color =
@@ -27,20 +27,16 @@ export function CategorySummaryView({ viewModel, props }: Props) {
         {categoryName}
       </Text>
       <Text size={18} weight="500" color={colors.black[900]}>
-        {formatAmount(currentMonth)}
+        {currentMonth}
       </Text>
       <Text size={14} color={colors.black[600]}>
-        {formatAmount(difference)}
+        {difference}
       </Text>
       <styled.ContainerDifference>
         <Text weight="500" color={color} style={{ marginRight: 4 }}>
-          {Math.floor(percent)}%
+          {percent}
         </Text>
-        {percent < 0 ? (
-          <Icon name="trendDown" color={color} />
-        ) : (
-          <Icon name="trendUp" color={color} />
-        )}
+        {getIcon(color)}
       </styled.ContainerDifference>
     </styled.CategorySummary>
   );
