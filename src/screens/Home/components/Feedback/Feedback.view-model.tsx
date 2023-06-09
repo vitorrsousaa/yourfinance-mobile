79ceb0d4c2fe7f145c977308d9ab5e8328482568
Vitor Sousa <v.sousa.cf@gmail.com>
2023-06-09 19:52:08 -0300

    fix: Updated styles of feedback

--- src/screens/Home/components/Feedback/Feedback.view-model.tsx
+++ src/screens/Home/components/Feedback/Feedback.view-model.tsx
@@ -1,13 +1,19 @@
 import { ReactElement, useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
 
 import Icon from '../../../../components/Icons';
+import FeedbacksService from '../../../../service/FeedbacksService';
 
 export interface FeedbackViewModelProps {
   options: OptionProps[];
   selectedFeedback: string;
   description: string;
+  isLoading: boolean;
+  isError: boolean;
+  isSubmitted: boolean;
   setDescription: (description: string) => void;
   setSelectedFeedback: (feedback: string) => void;
+  handleSubmit: () => Promise<void>;
 }
 
 interface OptionProps {
@@ -19,6 +25,14 @@ interface OptionProps {
 export function FeedbackViewModel() {
   const [selectedFeedback, setSelectedFeedback] = useState('');
   const [description, setDescription] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const { isLoading, isError, mutateAsync } = useMutation({
+    mutationFn: () =>
+      FeedbacksService.register({ selectedFeedback, description }),
+    onSuccess: () => setIsSubmitted(true),
+    onError: () => console.log('Erro'),
+  });
 
   const options: OptionProps[] = [
     {
@@ -38,11 +52,19 @@ export function FeedbackViewModel() {
     },
   ];
 
+  async function handleSubmit() {
+    await mutateAsync();
+  }
+
   return {
     options,
     selectedFeedback,
-    setSelectedFeedback,
     description,
+    isLoading,
+    isError,
+    isSubmitted,
+    handleSubmit,
+    setSelectedFeedback,
     setDescription,
   };
 }
--- src/screens/Home/components/Feedback/Feedback.view.tsx
+++ src/screens/Home/components/Feedback/Feedback.view.tsx
@@ -21,68 +21,88 @@ export function FeedbackView({ viewModel, props }: Props) {
     options,
     selectedFeedback,
     description,
+    isLoading,
+    isSubmitted,
     setDescription,
     setSelectedFeedback,
+    handleSubmit,
   } = viewModel;
 
   const { colors } = useTheme();
 
   return (
     <styled.Feedback {...feedbackProps} style={{ padding: 24 }}>
-      <View>
-        <Text weight="500" size={18}>
-          Avalie a experiência
-        </Text>
-        <Text size={14} color={colors.black[600]} style={{ marginTop: 8 }}>
-          O que você achou dessa nova experiência?
-        </Text>
-      </View>
+      {isSubmitted ? (
+        <View style={{ gap: 24 }}>
+          <styled.Title>
+            Agradecemos por dedicar
+            <styled.Title color={colors.green[500]}>
+              {' o seu tempo'}
+            </styled.Title>
+          </styled.Title>
+          <Text>
+            Vamos analisar seu comentário para deixar o yourFinance cada vez
+            melhor para você.
+          </Text>
+        </View>
+      ) : (
+        <>
+          <View>
+            <styled.Title>Avalie a experiência</styled.Title>
+            <Text size={14} color={colors.black[600]} style={{ marginTop: 8 }}>
+              O que você achou dessa nova experiência?
+            </Text>
+          </View>
 
-      <FlatList
-        data={options}
-        style={{ marginTop: 24, marginBottom: 12 }}
-        scrollEnabled={false}
-        keyExtractor={(item) => item.label}
-        renderItem={({ item }) => {
-          const color =
-            selectedFeedback !== item.value
-              ? colors.black[500]
-              : colors.green[400];
-          return (
-            <TouchableOpacity
-              onPress={() => setSelectedFeedback(item.value)}
-              activeOpacity={0.9}
-              style={{ alignItems: 'center', width: 90 }}
-            >
-              {item.icon(color)}
-              <Text weight="500" color={color}>
-                {item.label}
-              </Text>
-            </TouchableOpacity>
-          );
-        }}
-        contentContainerStyle={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      />
+          <FlatList
+            data={options}
+            style={{ marginTop: 24, marginBottom: 12 }}
+            scrollEnabled={false}
+            keyExtractor={(item) => item.label}
+            renderItem={({ item }) => {
+              const color =
+                selectedFeedback !== item.value
+                  ? colors.black[500]
+                  : colors.green[400];
+              return (
+                <TouchableOpacity
+                  onPress={() => setSelectedFeedback(item.value)}
+                  activeOpacity={0.9}
+                  style={{ alignItems: 'center', width: 90 }}
+                >
+                  {item.icon(color)}
+                  <Text weight="500" color={color}>
+                    {item.label}
+                  </Text>
+                </TouchableOpacity>
+              );
+            }}
+            contentContainerStyle={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          />
 
-      <InputOutlined
-        placeholder="O que poderia melhorar? (opcional)"
-        returnKeyType="send"
-        autoCorrect={false}
-        value={description}
-        onChangeText={setDescription}
-      />
+          <InputOutlined
+            placeholder="O que poderia melhorar? (opcional)"
+            returnKeyType="send"
+            autoCorrect={false}
+            value={description}
+            onChangeText={setDescription}
+          />
 
-      <Button
-        variant="primary"
-        style={{ width: 140 }}
-        disabled={!selectedFeedback}
-      >
-        Enviar
-      </Button>
+          <Button
+            variant="primary"
+            style={{ width: 140 }}
+            disabled={!selectedFeedback}
+            loading={isLoading}
+            onPress={handleSubmit}
+          >
+            Enviar
+          </Button>
+        </>
+      )}
     </styled.Feedback>
   );
 }
